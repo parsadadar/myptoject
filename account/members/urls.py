681a149f3d0519372702f1e4bbe41c7756382c00
parsadadar <parsahit@gmail.com>
2022-08-14 07:49:37 -0700

    new road to  home page
    ss

--- account/members/urls.py
+++ account/members/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('adm/', views.adm,name='admin_site'),
-    path('',views.spec),
+    path('',views.spec,name='1313'),
     path('<int:cod>/', views.special_num),
     path('<str:cod>/', views.special,name='010123'),
 ]
